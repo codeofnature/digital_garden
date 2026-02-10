a32ab0fb5723076f6a1529b5c0c68bdb0fec6096
kodnatury <43938884+codeofnature@users.noreply.github.com>
2026-02-10 10:09:50 +0100

    Quartz sync: Feb 10, 2026, 10:09 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,15 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Code of Nature",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    locale: "pl-PL",
+    baseUrl: "codeofnature.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
